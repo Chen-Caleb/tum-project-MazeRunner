e437edb8a2e02b3543706bec206fa9ec1ecd99a9
Chen Zhang <chen.zhang@tum.de>
2025-01-16 11:16:34 +0100

    Creat exit Button

--- core/src/de/tum/cit/fop/maze/MenuScreen.java
+++ core/src/de/tum/cit/fop/maze/MenuScreen.java
@@ -6,11 +6,13 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import de.tum.cit.fop.maze.screensInMenu.SelectMapScreen;
@@ -65,6 +67,17 @@ public class MenuScreen implements Screen {
                 game.setScreen(new SelectMapScreen(game)); // 跳转到新界面
             }
         });
+
+        //Creat exit Button
+        TextButton exitButton = new TextButton("Exit Game", game.getSkin());
+        exitButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                Gdx.app.exit();
+            }
+        });
+
+        table.add(exitButton).width(300).padBottom(10).row();
 
 
     }
