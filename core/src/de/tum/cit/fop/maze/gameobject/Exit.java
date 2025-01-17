ac7c17381575774ca68854295706435ad9cdab26
Chen Zhang <chen.zhang@tum.de>
2025-01-17 10:28:12 +0100

    Exit

--- core/src/de/tum/cit/fop/maze/gameobject/Exit.java
+++ core/src/de/tum/cit/fop/maze/gameobject/Exit.java
@@ -1,15 +1,23 @@
 package de.tum.cit.fop.maze.gameobject;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Rectangle;
 
 public class Exit extends StaticObject{
+    private TextureRegion exitTextureRegion;
 
     public Exit(float x, float y) {
         super(x, y);
+        width = 16;
+        height = 16;
+        rectangle = new Rectangle(position.x, position.y, width, height);
+        exitTextureRegion = new TextureRegion(basictiles, 2 * width, 6 * height, width, height);
+
     }
 
     @Override
     public void render(SpriteBatch spriteBatch) {
-
+        spriteBatch.draw(exitTextureRegion, rectangle.x, rectangle.y);
     }
 }
